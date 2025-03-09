188678af02af7cdd269c7e1c35fc144f93f87e91
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-03-10 01:19:02 +0530

    changes in initksocket.c

--- Assignment_4/grok/initksocket.c
+++ Assignment_4/grok/initksocket.c
@@ -1,55 +1,4 @@
-// #include "ksocket.h"
-// #include <pthread.h>
-// #include <signal.h>
 
-// static volatile int keepRunning = 1;
-
-// void intHandler(int dummy) {
-//     (void) dummy;
-//     keepRunning = 0;
-// }
-
-// int main() {
-//     signal(SIGINT, intHandler);
-
-//     int shmid = init_shared_memory();
-//     if (shmid == -1) {
-//         fprintf(stderr, "[initksocket] Failed to initialize shared memory\n");
-//         return 1;
-//     }
-
-//     pthread_t thread_r_id, thread_s_id;
-//     ThreadArgs args = { shmid };
-//     if (pthread_create(&thread_r_id, NULL, thread_r, &args) != 0) {
-//         perror("[initksocket] pthread_create failed for thread_r");
-//         cleanup_shared_memory(shmid);
-//         return 1;
-//     }
-//     if (pthread_create(&thread_s_id, NULL, thread_s, &args) != 0) {
-//         perror("[initksocket] pthread_create failed for thread_s");
-//         pthread_cancel(thread_r_id);
-//         pthread_join(thread_r_id, NULL);
-//         cleanup_shared_memory(shmid);
-//         return 1;
-//     }
-
-//     printf("[initksocket] Shared memory created with ID: %d\n", shmid);
-//     printf("[initksocket] Threads R and S started\n");
-//     printf("[initksocket] Run user1 and user2 with shmid=%d\n", shmid);
-//     printf("[initksocket] Press Ctrl+C to exit\n");
-
-//     while (keepRunning) {
-//         sleep(1);
-//     }
-
-//     printf("[initksocket] Cleaning up...\n");
-//     pthread_cancel(thread_r_id);
-//     pthread_cancel(thread_s_id);
-//     pthread_join(thread_r_id, NULL);
-//     pthread_join(thread_s_id, NULL);
-//     cleanup_shared_memory(shmid);
-//     return 0;
-// }
 
 
 #include "ksocket.h"
@@ -65,7 +14,7 @@ void intHandler(int dummy) {
 }
 
 int main() {
-    srand(time(NULL)); // Seed random number generator for dropMessage()
+    srand(time(NULL)); 
     signal(SIGINT, intHandler);
 
     int shmid = init_shared_memory();
