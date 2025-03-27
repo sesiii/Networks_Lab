63c86f416293e8c63756c948cd9440be91d30d62
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-03-27 22:21:19 +0530

    Introduced testing.c to the project (2025-03-27 22:21:19)

---
+++ LabTests/labtest2/testing.c
@@ -0,0 +1,32 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<string.h>
+// #include<arpa/inet.h>
+// #include<unistd.h>
+
+// #define SERVER_IP "127.0.0.1"
+// #define PORT 8080
+// #define BUFFER_SIZE 1024
+
+// int main()
+// {
+//     int sock_fd;
+//     struct sockaddr_in server_addr;
+//     char buffer[BUFFER_SIZE];
+//     socklen_t addr_len=sizeof(server_addr);
+
+//     sock_fd=socket(AF_INET, SOCK_DGRAM,0);
+//     if(sock_fd<0){
+//         perror("Socket creation failed.\n");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     memset(&server_addr,0,sizeof(server_addr));
+//     server_addr.sin_family=AF_INET;
+//     server_addr.sin_port=htons(PORT);
+//     inet_pton(AF_INET,SERVER_IP,&server_addr.sin_addr);
+
+
+// }
+
+#
