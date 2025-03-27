247613a67104f73636b1f6c45baa05e624858f4e
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-03-27 22:44:52 +0530

    `udp sock setup` (2025-03-27 22:44:48)

--- LabTests/labtest2/testing.c
+++ LabTests/labtest2/testing.c
@@ -1,11 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+
+// // #define SERVER_IP "127.0.0.1"
+// // #define PORT 8080
+// // #define BUFFER_SIZE 1024
+
+// // int main()
+// // {
+// //     int sock_fd;
+// //     struct sockaddr_in server_addr;
+// //     char buffer[BUFFER_SIZE];
+// //     socklen_t addr_len=sizeof(server_addr);
+
+// //     sock_fd=socket(AF_INET, SOCK_DGRAM,0);
+// //     if(sock_fd<0){
+// //         perror("Socket creation failed.\n");
+// //         exit(EXIT_FAILURE);
+// //     }
+
+// //     memset(&server_addr,0,sizeof(server_addr));
+// //     server_addr.sin_family=AF_INET;
+// //     server_addr.sin_port=htons(PORT);
+// //     inet_pton(AF_INET,SERVER_IP,&server_addr.sin_addr);
+
+
+// // }
+
 // #include<stdio.h>
 // #include<stdlib.h>
 // #include<string.h>
-// #include<arpa/inet.h>
 // #include<unistd.h>
+// #include<arpa/inet.h>
 
 // #define SERVER_IP "127.0.0.1"
-// #define PORT 8080
+// #define port 8080
 // #define BUFFER_SIZE 1024
 
 // int main()
@@ -15,7 +46,7 @@
 //     char buffer[BUFFER_SIZE];
 //     socklen_t addr_len=sizeof(server_addr);
 
-//     sock_fd=socket(AF_INET, SOCK_DGRAM,0);
+//     sock_fd=socket(AF_INET,SOCK_DGRAM,0);
 //     if(sock_fd<0){
 //         perror("Socket creation failed.\n");
 //         exit(EXIT_FAILURE);
@@ -23,17 +54,13 @@
 
 //     memset(&server_addr,0,sizeof(server_addr));
 //     server_addr.sin_family=AF_INET;
-//     server_addr.sin_port=htons(PORT);
+//     server_addr.sin_port=htons(port);
 //     inet_pton(AF_INET,SERVER_IP,&server_addr.sin_addr);
-
+    
 
 // }
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<arpa/inet.h>
+
 
 #define SERVER_IP "127.0.0.1"
 #define port 8080
@@ -44,18 +71,17 @@ int main()
     int sock_fd;
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
-    socklen_t addr_len=sizeof(server_addr);
+    socklen_t addr_len = sizeof(server_addr);
 
-    sock_fd=socket(AF_INET,SOCK_DGRAM,0);
+    sock_fd=socket(AF_INET, SOCK_DGRAM,0);
     if(sock_fd<0){
         perror("Socket creation failed.\n");
         exit(EXIT_FAILURE);
     }
 
-    memset(&server_addr,0,sizeof(server_addr));
+    memset(&server_addr,0, sizeof(server_addr));
     server_addr.sin_family=AF_INET;
     server_addr.sin_port=htons(port);
     inet_pton(AF_INET,SERVER_IP,&server_addr.sin_addr);
     
-
 }
