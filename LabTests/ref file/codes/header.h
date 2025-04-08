0d2bc906d2c572096c3a36e59eb210a08729a1c6
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-04-08 16:10:11 +0530

    Introduced header.h to the project (2025-04-08 16:10:11)

---
+++ LabTests/ref file/codes/header.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#define PORT 8080
+#define BUFFER_SIZE 1024
+#define MAX_CLIENTS 10
